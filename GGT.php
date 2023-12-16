493a8dba321230216b936e19feea9c5db941261b
Robin Rütter <robin.ruetter@stud.edubs.ch>
2023-12-16 14:49:35 +0100

    Add function berechneGgt

--- GGT.php
+++ GGT.php
@@ -5,3 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fibonacci-Reihe</title>
 </head>
+<body>
+<?php
+
+function berechneGgt($a, $b) {
+    // Solange der Rest nicht gleich 0 ist, setze $b auf $a und $a auf den Rest
+    while ($b != 0) {
+        $rest = $a % $b;
+        $a = $b;
+        $b = $rest;
+    }
+    // Wenn $b gleich 0 ist, dann ist $a der ggT
+    return $a;
+}
+
+?>
+</body>
+</html>
